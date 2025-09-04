5059e0171b9bbbe94bce721bf6109ca6a1efcadd
Freddie <143739931+hippogriff101@users.noreply.github.com>
2025-09-04 17:58:23 +0000

    Updates bot.py and hides beta/

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
-# bot.py
+#Install the required packages before running the bot:
 import os
 from discord.ext import commands
 from discord import Intents
 from dotenv import load_dotenv
 from jokeapi import Jokes
 
-# Make get_jokes an async function
+#This is the function that talks to JokeAPI to find a joke
 async def get_jokes():
     j = await Jokes()
     joke = await j.get_joke(lang="en", blacklist=['nsfw', 'racist', 'sexist', 'explicit'])
@@ -14,19 +14,20 @@ async def get_jokes():
     else:
         return f'{joke["setup"]}\n{joke["delivery"]}'
 
-# Load environment variables
+#Make sure you have a .env file with your DISCORD_TOKEN variable set
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Set up bot
+#Set up the bot with the command prefix and intents
 intents = Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+#This is the command that the bot will respond to
 @bot.command(name='joke')
 async def joke(ctx):
     response = await get_jokes()
     await ctx.send(response)
 
-# Run bot
+#Run the bot
 bot.run(TOKEN)
